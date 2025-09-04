73d78965df00d5dc3f77ff7fc74f6422c478fb79
Adam Horn <mr.amhorn@gmail.com>
2025-09-04 15:36:14 -0700

    updating app.config file

--- app.config.js
+++ app.config.js
@@ -1,3 +1,10 @@
 import { defineConfig } from "@solidjs/start/config";
+import solidPlugin from 'vite-plugin-solid';
 
-export default defineConfig({});
+export default defineConfig({
+  plugins: [solidPlugin()],
+  base: '/project-name/',  // <-- replace with your actual repo name
+  build: {
+    outDir: 'dist',
+  },
+});
